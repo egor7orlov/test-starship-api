0bc8ac760156295bbbf9ba6d66e81ab59efa55ff
Yehor Orlov <egor7orlov@proton.me>
2024-01-16 12:57:24 +0100

    Array of use cases type restriction

--- src/use-cases/use-case.interface.ts
+++ src/use-cases/use-case.interface.ts
@@ -1,3 +1,7 @@
 export interface UseCase<InputArgs extends Record<string, any>, Result> {
   execute(argsObj: InputArgs): Result;
 }
+
+export type UseCaseClass<InputArgs extends Record<string, any>, Result> = new (
+  ...args: unknown[]
+) => UseCase<InputArgs, Result>;
--- src/use-cases/use-cases.module.ts
+++ src/use-cases/use-cases.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LangChainModule } from '../domain/lang-chain/lang-chain.module';
-import { ProcessTextUseCase } from './text-processing/./process-text.use-case';
+import { ProcessTextUseCase } from './text-processing/process-text.use-case';
+import { UseCaseClass } from './use-case.interface';
 
-const USE_CASES = [ProcessTextUseCase];
+const USE_CASES: Array<UseCaseClass<Record<string, unknown>, unknown>> = [
+  ProcessTextUseCase,
+];
 
 @Module({
   imports: [LangChainModule],
